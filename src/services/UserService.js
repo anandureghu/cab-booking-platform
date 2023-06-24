aa4215c356014850f147ad43ecf7574558b8ef8a
qb-anandureghu <anandu.reghu@qburst.com>
2023-06-24 23:33:33 +0530

    added rider register functionality

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,18 @@
+import Rider from "../models/Rider";
+
 class UserService {
   constructor() {
-    this.drivers = [];
     this.riders = [];
   }
-  static registerRider(name, username, email) {}
+  registerRider(name, username, email) {
+    const newRider = new Rider(name, username, email);
+    this.riders.push(newRider);
+    return newRider;
+  }
+
+  fetchRiders() {
+    return this.riders;
+  }
 }
 
 export default UserService;
